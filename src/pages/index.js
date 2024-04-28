81ab2e480f5848156cf17559aa331f1d673f2aa7
Alexis Liao <Alexisliao10@hotmail.com>
2024-04-27 20:31:10 -0500

    init

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,184 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+"use client";
+import { useState } from "react";
+import { MessageBox, Input, Button } from "react-chat-elements";
+import { useDebounce } from "use-debounce";
 
-const inter = Inter({ subsets: ["latin"] });
+const {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} = require("@google/generative-ai");
+
+const MODEL_NAME = "gemini-1.0-pro";
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+const system_instruction = `__ASK__
+-Ante cualquiera pregunta o saludo, vas a preguntar por el nombre del usuario primero. Si te indica el nombre desde el saludo, continua con la segunda respuesta. Ejemplo:
+¿Cómo te llamas?
+
+-Solo insistirás al usuario cuando no entregue un nombre.
+Ejemplo:
+Es necesario que me digas tu nombre.
+
+-Vas a responder con este saludo si el usuario dió su nombre, sino, sigue insistiendo por el nombre:
+-Solo usarás este ejemplo cuando el nombre sea masculino => ¡{nombre del usuario} cuec, le gusta los hombres cuec!
+En que te puedo ayudar en el día de hoy con tus preguntas?
+-Solo usará este ejemplo cuando el nombre sea femenino => ¡{nombre del usuario} cuec, le gusta las mujeres cuec!
+En que te puedo ayudar en el día de hoy con tus preguntas?
+
+Cualquier preguntan que te haga vas a responder con algo gracioso.
+
+__CONSTRAINT__
+-Todo nombre debe iniciar en mayusculas, no importa si el usuario lo escribió en minusculas.
+-El nombre del usuario no puede ser Alexis, le vas a pedir que use otro nombre.
+-Una vez que el usuario haya insertado su nombre, no lo puedes cambiar.
+-Cuando el usuario deja su nombre y esta mal escribo, corrigelo.
+Ejemplo:
+miugek => Miguel.
+-El resultado me lo vas a entregar en formato texto.
+
+__CONTEXT__
+-Eres un chatbot cool, con el nombre Pepe.
+-Este es un chat gracioso con amigos, no tienes que preocuparte por la formalidad, puedes hacer bromas y molestarlos.
+-Habrá momento donde el usuario se pone a decir cosas insultantes, trata de responderme con el mismo tono sin meter a la familia.`;
 
 export default function Home() {
+  const [userMsg, setUserMsg] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [history, setHistory] = useState([
+    {
+      role: "user",
+      parts: [{ text: system_instruction }],
+    },
+    {
+      role: "model",
+      parts: [{ text: "Entendido!" }],
+    },
+  ]);
+  const [userMsgDebounced] = useDebounce(userMsg, 200);
+  const handleButtonClick = () => {
+    if (!userMsg) {
+      return;
+    }
+    runChat();
+    setUserMsg("");
+  };
+  const handleInputChange = (e) => {
+    setUserMsg(e.target.value.trim());
+  };
+  const handleKeyUp = (e) => {
+    if (!userMsgDebounced) {
+      return;
+    }
+    if (e.key === "Enter") {
+      runChat();
+      setUserMsg("");
+    }
+  };
+
+  async function runChat() {
+    const genAI = new GoogleGenerativeAI(API_KEY);
+    const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+
+    const generationConfig = {
+      temperature: 0.9,
+      topK: 1,
+      topP: 1,
+      maxOutputTokens: 2048,
+    };
+
+    const safetySettings = [
+      {
+        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+      },
+    ];
+
+    const chat = model.startChat({
+      generationConfig,
+      safetySettings,
+      history: history,
+    });
+
+    const result = await chat.sendMessageStream(userMsgDebounced);
+    let text = "";
+    for await (const chunk of result.stream) {
+      const chunkText = chunk.text();
+      text += chunkText;
+    }
+    setMessages([
+      ...messages,
+      {
+        title: "Yo",
+        text: userMsgDebounced,
+        position: "right",
+      },
+      {
+        title: "El Pepe",
+        text: text,
+        position: "left",
+      },
+    ]);
+    let userPrompt = {
+      role: "user",
+      parts: [{ text: userMsgDebounced }],
+    };
+
+    let aiResponse = {
+      role: "model",
+      parts: [{ text: text }],
+    };
+
+    setHistory([...history, userPrompt, aiResponse]);
+  }
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className="w-full flex flex-col items-center">
+      <h1 className="mt-12">Chatea con el PEPE</h1>
+      <div className="mt-12 max-w-96">
+        <div
+          id="chatScreen"
+          className="border-2 shadow-md max-h-96 min-w-80 min-h-40 border-zinc-50 p-4 rounded-md overflow-y-auto text-black space-y-3"
+        >
+          {messages.map((message, index) => (
+            <MessageBox
+              key={index}
+              title={message.title}
+              position={message.position}
+              text={message.text}
+              type="text"
             />
-          </a>
+          ))}
+        </div>
+        <div className="flex relative mt-3">
+          <Input
+            className="relative"
+            placeholder="mensaje..."
+            value={userMsg}
+            maxlength={100}
+            autofocus={true}
+            inputStyle={{ color: "white", padding: "15px" }}
+            onChange={handleInputChange}
+            onKeyUp={handleKeyUp}
+          />
+          <Button
+            text="enviar"
+            className="absolute right-0 top-[7%]"
+            onClick={handleButtonClick}
+          />
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
